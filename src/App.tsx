ca3c558bc8aee7d025a3af632eeb138fd4b7a4e5
nattzchukwumela <chukwumelapraise@gmail.com>
2024-07-25 12:03:10 +0100

    realtime messaging works effectively and they render in ascending order

--- src/App.tsx
+++ src/App.tsx
@@ -7,56 +7,65 @@ import Auth from './component/Auth';
 import Chat from './component/Chat';
 
 interface UserDetail {
-  result: UserCredential | null;
+	result: UserCredential | null;
 	signIn: () => void;
 }
 
-	const App: React.FC = () => {
-  const [userDetail, setUserDetail] = useState<UserDetail>({ result: null, signIn: () => {} });
-	const [room, setRoom] = useState<null>(null);
-  const roomInputRef = useRef<HTMLInputElement>(null);
+const App: React.FC = () => {
+	const [userDetail, setUserDetail] = useState<UserDetail>({ result: null, signIn: () => { } });
+	const [room, setRoom] = useState<string>("");
+	const roomInputRef = useRef<HTMLInputElement>(null);
 	const cookies = new Cookies();
 	const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
 
-	console.log(userDetail);
+	const signInWithGoogle = async () => {
+		try {
+			const result = await signInWithPopup(auth, provider);
+			setUserDetail({ result, signIn: signInWithGoogle });
+			if (result && 'user' in result) {
+				cookies.set("auth-token", result.user.refreshToken);
+				setIsAuth(true);
+			}
+		} catch (error) {
+			console.error("Error signing in with Google", error);
+		}
+	}
 
-  const signInWithGoogle = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      setUserDetail({ result, signIn: signInWithGoogle });
-      if (result && 'user' in result) {
-        cookies.set("auth-token", result.user.refreshToken);
-        setIsAuth(true);
-      }
-    } catch (error) {
-      console.error("Error signing in with Google", error);
-    }
-  }
+	const signUserOut = (): void => {
+
+	}
 
 	if (!isAuth) {
 		return <Auth signIn={signInWithGoogle} />;
 	}
 	return (
-		<div>
-			{room ? <Chat room={room} /> :
-       <div className='room'>
-        <label>
-          Enter Room Chat
-        </label>
+		<>
+			<div>
+				{room ? (
+					<Chat room={room.toLowerCase()} />
+				) :
+					<div className='room'>
+						<label>
+							Enter Room Chat
+						</label>
 						<input
 							type="text"
 							placeholder="Room"
-							ref={roomInputRef}/>
-					<button
-						onClick={() => {
-              if (roomInputRef.current && roomInputRef.current.value) {
-                setRoom(roomInputRef.current.value as any);
-              }              
-						}}>
-						Enter Chat
-					</button>
-        </div>}
-		</div>
+							ref={roomInputRef} />
+						<button
+							onClick={() => {
+								if (roomInputRef.current && roomInputRef.current.value) {
+									setRoom(roomInputRef.current.value as any);
+								}
+							}}>
+							Enter Chat
+						</button>
+					</div>}
+			</div>
+			<div className='sign-out' onClick={signUserOut}>
+				sign out
+			</div>
+		</>
 	);
 }
 
--- src/component/Chat.tsx
+++ src/component/Chat.tsx
@@ -1,11 +1,11 @@
 import React, { FormEvent, useEffect, useState } from 'react'
-import { addDoc, getDoc, deleteDoc, collection, serverTimestamp, onSnapshot, query, where } from 'firebase/firestore'
+import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy } from 'firebase/firestore'
 import { auth, db } from '../config/firebase-config';
 import '../styles/Chat.css';
 
 
 type Props = {
-	room: string;
+  room: string;
 }
 function Chat({ room }: Props) {
 
@@ -14,59 +14,63 @@ function Chat({ room }: Props) {
   const messagesRef = collection(db, "messages");
 
   useEffect(() => {
-		const queryMessages = query(messagesRef, where("room", "==", room));
-    
-    onSnapshot(queryMessages, (snapshot) => { 
-			let messages: any[] = [];
-			snapshot.forEach((doc) => {
-				messages.push({ ...doc.data(), id: doc.id });
-			});
-			setMessage(messages);
+    const queryMessages = query(messagesRef, where("room", "==", room), orderBy("createdAt"));
+
+    const unsubscribe = () => onSnapshot(queryMessages, (snapshot) => {
+      let messages: any[] = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessage(messages);
     })
-		
+
+    return unsubscribe();
   }, [messagesRef, room])
 
-  async function handleSubmit(e: FormEvent){
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault()
-    if(newMessage === "") return
+    if (newMessage === "") return
     await addDoc(messagesRef, {
-    text: newMessage,
-    createdAt: serverTimestamp(),
-    user: auth.currentUser?.displayName,
-    room: room
-  })
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser?.displayName,
+      room: room
+    })
 
-  setNewMessage("");
+    setNewMessage("");
   }
-  
+
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-      setNewMessage(e.target.value);
+    setNewMessage(e.target.value);
   }
 
 
   return (
-  <div className='chat-app'>
-  <div>
-				{message.map((message) => {
-					return (
-						<div key={message.id}>
-							<p>{message.text}</p>
-							<p>{message.user}</p>
-						</div>
-					);
-				})}
-  </div>
-  <form onSubmit={handleSubmit} className='new-message-form'>
-  <input type="text"
-  className="new-message-input" 
-  onChange={handleChange}
-  value={newMessage}
-  placeholder='type your message here' />
-  <button type='submit'
-  className="send-button"
-  >send</button>
-  </form>
-  </div>
+    <div className='chat-app'>
+      <div className="header">
+        <h1>Welcome to: {room.toUpperCase()}</h1>
+        <div className='messages'>
+          {message.map((message) => {
+            return (
+              <div key={message.id} className='message'>
+                <span className='user'>{message.user}</span>
+                {message.text}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <form onSubmit={handleSubmit} className='new-message-form'>
+        <input type="text"
+          className="new-message-input"
+          onChange={handleChange}
+          value={newMessage}
+          placeholder='type your message here' />
+        <button type='submit'
+          className="send-button"
+        >send</button>
+      </form>
+    </div>
   )
 }
 
